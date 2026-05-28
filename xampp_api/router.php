24bcc8fa269e8ac3a493a5491e29fb96139ad117
shinchan <sricharanbharadwaj810@gmail.com>
2026-05-29 00:00:59 +0530

    fix: use Ubuntu PHP instead of Apache

--- xampp_api/router.php
+++ xampp_api/router.php
@@ -1,6 +1,9 @@
 <?php
-$file = __DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-if (is_file($file)) {
-    return false; // serve the file directly
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$file = __DIR__ . $uri;
+
+if ($uri !== '/' && file_exists($file) && !is_dir($file)) {
+    return false;
 }
+
 require __DIR__ . '/api.php';
